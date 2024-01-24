0e9fdef2ae16aa036f10e34913cc5bc2e2137bfa
Fauzan Ibrahim <fauzan.ibrahim21@gmail.com>
2024-01-24 21:04:41 +0700

    Complete mux selector in control unit instantiations

--- rtl_experimental/top_level_architecture.v
+++ rtl_experimental/top_level_architecture.v
@@ -26,7 +26,7 @@ input clock
 //internal signal
 wire [15:0] mem_to_ALU_operand_1;
 wire [15:0] mem_to_operand2;
-wire [15:0] operand2;
+wire [15:0] operand2_ALU;
 wire [15:0] ALU_result;
 wire [15:0] mux_to_mem;
 wire [15:0] ReLu_to_mux;
@@ -53,7 +53,7 @@ counter_mem counter_mem_0(counter_input,counter,clock);
 CLA CLA_COUNTER (16'd1,counter,1'b0,counter_input);
 //demux_1_4 demux_2 (demux_to_mem,demux_to_sigmoid,demux_to_ReLu,demux_to_Sigmoid_diff,dest_control,ALU_result);
 mux_4_1 mux_2 (ALU_result,sigmoid_to_mux,ReLu_to_mux,sigmoid_diff_to_mux,dest_control,mux_to_mem);
-mux_2_1 operand2_ALU (mem_to_operand2, 16'd0, operand2_sel, )
+mux_2_1 mux_operand2 (mem_to_operand2, 16'd0, operand2_sel, )
 ReLu ReLu_0 (ALU_result,ReLu_to_mux);
 sigmoid_diff_lut sigmoid_diff_lut_0 (ALU_result,sigmoid_diff_to_mux);
 sigmoid_lut sigmoid_lut_0 (ALU_result,sigmoid_to_mux);
@@ -61,7 +61,7 @@ sel_mem selector_read_1(clock,enable_sel_mem,instruction[11:8],sector_selector_r
 sel_mem selector_read_2(clock,enable_sel_mem,instruction[7:4],sector_selector_read_2);
 sel_mem selector_write(clock,enable_sel_mem,instruction[3:0],sector_selector_write);
 top_level_memory mem_0 (mux_to_mem,sector_selector_write,instruction[3:0],clock,write_enable_mem,instruction[11:8],instruction[7:4],sector_selector_read_1,sector_selector_read_2,mem_to_ALU_operand_1,mem_to_ALU_operand_2);
-CU CU_0 (instruction[15:12],write_enable_mem,enable_ALU,enable_sel_mem,dest_control,op_select);
+CU CU_0 (instruction[15:12],write_enable_mem,enable_ALU,enable_sel_mem,dest_control,op_select,operand2_sel);
 Instrcut_mem instruction_0 (clock,counter,instruction); 
 
 endmodule
