3ecabea193c9c909cda349c8f53f5d505adc6177
Teffandi <13221084@std.stei.itb.ac.id>
2024-01-27 14:38:47 +0700

    previous was wrong

--- rtl_experimental/CLA.v
+++ rtl_experimental/CLA.v
@@ -17,6 +17,7 @@ wire [3:0]Gen;
 wire [3:0]Ci;
 wire [3:0] C_OUT_temp;
 wire [15:0] sum_temp;
+wire overflow;
 
 four_bit_adder Adder_0 (ADD_1[3:0],ADD_2[3:0],sum_temp[3:0],Gen[0],C,Ci[0],C_OUT_temp[0],Prop[0]);
 four_bit_adder Adder_1 (ADD_1[7:4],ADD_2[7:4],sum_temp[7:4],Gen[1],Ci[0],Ci[1],C_OUT_temp[1],Prop[1]);
@@ -25,8 +26,12 @@ four_bit_adder Adder_3 (ADD_1[15:12],ADD_2[15:12],sum_temp[15:12],Gen[3],Ci[2],C
 
 wire C_out;
 assign C_out = Ci[3];
+assign overflow = C_out^C_OUT_temp;
 assign S[14:0] = sum_temp[14:0];
-assign S[15] = C_out^C_OUT_temp[3];
+assign S[15] = overflow|sum_temp[15];
+
+
+
 
 
 endmodule
