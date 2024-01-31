f6b79f1cdafc8d96e5ab672ad02a21bbd67f4e59
Fauzan Ibrahim <fauzan.ibrahim21@gmail.com>
2024-01-31 16:28:25 +0700

    Delete ModelSim Files

--- rtl_experimental/work/counter_mem/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity counter_mem is
-    port(
-        data_in         : in     vl_logic_vector(15 downto 0);
-        data_out        : out    vl_logic_vector(15 downto 0);
-        clk             : in     vl_logic
-    );
-end counter_mem;
--- rtl_experimental/work/top_level_architecture/_primary.vhd
+++
@@ -1,7 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity top_level_architecture is
-    port(
-        clock           : in     vl_logic
-    );
-end top_level_architecture;
